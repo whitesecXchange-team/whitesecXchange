650d4e5cd47eb4918e733d6d26ee2246c40dbfaf
bigMattBles <blestyawan@gmail.com>
2024-10-24 16:00:32 +0700

    fixed web.php to not be different

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', function () {
-    return view('registerchoice');
-});
+Route::get('/', function () {
+    return view('welcome');
+}
