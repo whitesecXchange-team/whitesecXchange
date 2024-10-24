ab60fd542b181e49593dc573f67355b7d9e8608d
oeuiaa <evangunawansec@gmail.com>
2024-10-24 15:36:01 +0700

    front-end updates
    nav, header, home

---
+++ resources/views/header.blade.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Nanum+Gothic:wght@400;700;800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Share+Tech+Mono&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('header')
+    <title>Home | whitesecXchange</title>
+</head>
+<body>
+    @include('nav')
+    <div class="bg">
+        
+    </div>
+</body>
+</html>
