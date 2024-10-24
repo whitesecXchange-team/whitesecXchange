7d19f4c70642e7e66f549e61d984325f963d653d
bigMattBles <blestyawan@gmail.com>
2024-10-24 15:38:18 +0700

    added Register Page

---
+++ resources/views/registerchoice.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#000000"/>
+    <title>White Hat</title>
+
+    <link rel="stylesheet" href="./css/registerchoice.css">
+</head>
+<body>
+    <div class="asking">
+        <p>Which One are You?</p>
+    </div>
+
+    <div class="registerChoices">
+        <button class="optionHack">
+            <div class="hookHack">
+                <p>Become a Bug Hunter:<br>
+                Secure, Solve, Succeed
+                </p>
+            </div>
+
+            <div class="sinHack">
+                <h2>Sign-up as Hacker</h2>
+            </div>
+        </button>
+
+        <button class="optionCompany">
+            <div class="hookComp">
+                <p>Looking for Cyber Defense?<br>
+                Post a Bounty Now!
+                </p>
+            </div>
+
+            <div class="sinComp">
+                <h2>Sign up as Company</h2>
+            </div>
+        </button>
+    </div>
+</body>
+</html>
