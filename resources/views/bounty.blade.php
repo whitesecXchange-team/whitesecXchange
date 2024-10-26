2cfeed311a7bbf944d53af7224db014d4f44050b
unknown <dickyprstyo09@gmail.com>
2024-10-26 19:13:08 +0700

    revisibountyblade

--- resources/views/bounty.blade.php
+++ resources/views/bounty.blade.php
@@ -1,119 +1,158 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#000000"/>
-    <meta name="description" content="W" />
-    <link rel="stylesheet" type="text/css" href="./css/bounty.css"/>
+    <meta charset="utf-8" />
     <title>White Hat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Web site created using create-react-app" />
+    <img alt="Logo" src="https://appproject.dhiwise.com/dhiwise-logo.png?c=&v=" style="width: 0px; height: 0px; display: none" />
     
+    <link rel="stylesheet" type="text/css" href="./css/index.css" />
+    <link rel="stylesheet" type="text/css" href="./css/font.css" />
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="./css/components.css" />
+    <link rel="stylesheet" type="text/css" href="./css/Bounty.css" />
 </head>
 <body>
     <div class="bounty">
-        <div class="column_one">
-            <div class="columnsearch container-xs">
-                <label class="search ui input gray_600 size-md underline square">
-                    <input name="search" type="text"/>
-                </label>
-                <div class="bountypost">
-                    <div class="bountypost-1 container-xs">
-                        <div class="rowlineseven">
-                            <div >
-                                <div class="rowprice-4">
-                                    <div class="rowbountytitle-4">
-                                        <p class="bountytitle-4 ui text size-title">Bounty Title</p>
-                                        <p class="company-4 ui text size-base">Company</p>
-
-
-                                    </div>
-                                        <p class="theultimate ui text size-title">$$$</p>
-
-
-                                </div>
-                                    <div class="lineseven_one">
-
-                                    </div>
-                            </div>
-                                <p class="bounty-1 ui text size-base">Bounty Description</p>
-
+        <header class="navbar">
+            <div class="column-1">
+                <div class="rowlineseven">
+                    <div class="columnwhitesec-1">
+                        <div class="rowwhitesec">
+                            <a href="https://www.youtube.com/embed/bv8Fk8Qsz7I" target="_blank">
+                                <p class="whitesec-1 text">
+                                    <span class="whitesec-span">whitesec&nbsp;</span>
+                                    <span class="whitesec-span-1">| xchange</span>
+                                </p>
+                            </a>
                         </div>
-                            <div class ="bountypost-2">
-                                <div class ="columnbountytit"></div>
-                                <div class="rowprice">
-                                <div class="rowbountytitle">
-                                    <p class ="bountytitle ui text">Bounty Title</p>
-                                    <p class="company ui text">Company</p>
-
+                    </div>
+                    
+                    <div class="columnlogin-1">
+                        <div class="login">
+                            <div class="rowlogin-1">
+                                <div class="leaderboard-4">
+                                    <a href="https://www.youtube.com/embed/bv8Fk8Qsz7I" target="_blank">
+                                        <p class="login-4 ui text">login</p>
+                                    </a>
                                 </div>
-                                    <p class="price ui text">$$$</p>
-                                </div> 
-                                    <div class ="lineseven_one-2">
-                                        <p class ="bounty-5 ui text">Bounty Description</p>
-                                    </div>
-
                             </div>
-                            <div class ="bountypost-3">
-                                <div class="rowprice"></div>
-                                <div class="rowbountytitle"></div>
-                                <p class ="bountytitle ui text">Bounty Title</p>
-                                <p class ="company ui text">Company</p>
-                            </div>
-                                <p class="price ui text">$$$</p>
-
+                            <div class="lineone-one-1"></div>
+                            <a href="https://www.youtube.com/embed/bv8Fk8Qsz7I" target="_blank">
+                                <button class="register ui button">register</button>
+                            </a>
+                        </div>
                     </div>
-                    <div class="lineseve_one-2">
-
-                    </div>
-                    <p class ="bounty-2 ui text">Bounty Description</p>
-
+                    <p class="contactus-1 ui text">contact us</p>
                 </div>
-                    <div class ="bountypost-3">
-                        <div class ="rowprice">
-                            <div class="rowbountytitle">
-                                <p class ="bountytitle ui text">Bounty Title</p>
-                                <p class ="company ui text">Company</p>
-
-                            </div>
-                            <p class="price ui text">$$$</p>
-
-                        </div>
-                        <div class="lineseven_one-2">
-                            <p class="bounty-2 ui text">Bounty Description</p>
-
-                        </div>
-                        <div class="bountypost-3">
-                            <div class="rowprice">
-                                <div class="rowbountytitle">
-                                    <p class ="bountytitle ui text">Bounty Description</p>
-                                    <p class="company ui text">Company</p>
-
-
-                                </div>
-                                <p class="price ui text">$$$</p>
-
-                            </div>
-                            <div class="lineseven_one-2">
-                                <p class="bounty-2 ui text">Bounty Description</p>
-
-                            </div>
-
-                        </div>
-
-                    </div>
+                <div class="lineseven_one"></div>
             </div>
-            <footer class="footer">
-                <div class="columnlineseven">
-                    <div class="lineseven_three">
-
-                    </div>
-                    <p class="class-2024whitehatxch ui text size-textxs">2024</p>
-
+            <div class="columnbounties container-xs">
+    <div class="row_one">
+        <div class="row-1">
+            <div class="rowbounties-1">
+                <div class="rowbounties">
+                    <p class="bounties-2 ui text">BOUNTIES</p>
+                    <a href="#">
+                        <img src="public/images/img_icons.svg" alt="Icons" class="icons_one" />
+                    </a>
                 </div>
-
-            </footer>
+                <div class="aboutus-1">
+                    <p class="aboutus-2 ui text">ABOUT US</p>
+                    <a href="#">
+                        <img src="public/images/img_user_indigo_a700_01.svg" alt="User" class="user_one-1" />
+                    </a>
+                </div>
+            </div>
+            <div class="leaderboard-3">
+                <div class="rowleaderboard">
+                    <div class="leaderboard-4">
+                        <p class="report-1 ui text">LEADERBOARD</p>
+                    </div>
+                    <a href="#">
+                        <img src="public/images/img_icons_indigo_a700_01.svg" alt="Icons" class="file_one" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="rowreport">
+            <p class="report-1 ui text">REPORT</p>
+            <div class="columnfile_one-1">
+                <a href="#">
+                    <img src="public/images/img_group_493.svg" alt="File" class="file_one" />
+                </a>
+            </div>
         </div>
     </div>
-    
+</div>
+
+<header>
+    <div class="column_one">
+        <div class="columnsearch container-xs">
+            <label class="search ui input gray_600 size-md underline square">
+                <img src="public/images/img_settings_base_1.svg" alt="Settings" class="settings" />
+                <input name="search" type="text"/>
+                <img src="public/images/img_settings_base_2.svg" alt="Settings" class="settings" />
+</label>
+</div>
+</div>
+</div>
+</div>
+</header>
+<div class="bountypost">
+<div class="bountypost-1 container-xs">
+<div class="rowlineseven">
+<div>
+<div class="rowprice-4">
+<div class="rowbountytitle-4">
+<p class="bountytitle-4 ui text size-title">Bounty Title</p>
+<p class="company-4 ui text size-base">Company</p>
+</div>
+<p class="theultimate ui text size-title">$$$</p>
+</div>
+<div class="lineseven_one-6"></div>
+</div>
+</div>
+<p class="bounty-1 ui text size-base">Bounty Description</p>
+</div>
+<div class="bountypost-2">
+<div class="columnbountytit">
+<div class="rowprice">
+<div class="rowbountytitle">
+<p class="bountytitle ui text">Bounty Title</p>
+<p class="company ui text ">Company</p>
+</div>
+<p class="price ui text">$$$</p>
+</div>
+<div class="lineseven_one-2"></div>
+<p class="bounty-2 ui text">Bounty Description</p>
+</div>
+<div class="bountypost-3">
+    <div class="rowprice">
+        <div class="rowbountytitle">
+            <p class="bounytitle ui text">Bounty Title</p>
+            <p class="company ui text">Company</p>
+
+        </div>
+        <p class="price ui text">$$$</p>
+    </div>
+    <div class="lineseven_one-2"></div>
+    <p class="bounty-2 ui text">Bounty Description</p>
+</div>
+</div>
+</div>
+</div>
+<footer class ="footer">
+    <div class="columnlineseven">
+        <div class="lineseven_three"></div>
+        <p class="class-2024whitehatxch ui text size -texts">@2024 whitehat exchange all right reserved</p>
+
+    </div>
+
+
+</footer>
+</div>
 </body>
 </html>
