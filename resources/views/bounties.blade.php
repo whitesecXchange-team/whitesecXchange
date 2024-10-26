8ec10c01eb46640c90d71e6d01e256ee94c7bc55
oeuiaa <evangunawansec@gmail.com>
2024-10-26 20:12:41 +0700

    fixes

--- resources/views/bounties.blade.php
+++ resources/views/bounties.blade.php
@@ -6,6 +6,63 @@
 </head>
 @include('nav')
 <body>
-    <h1>BOUNTIES</h1>
+    <div class="bounty">
+    <header>
+        <div class="column_one">
+            <div class="columnsearch container-xs">
+                <label class="search ui input gray_600 size-md underline square">
+                    <img src="public/images/img_settings_base_1.svg" alt="Settings" class="settings" />
+                    <input name="search" type="text"/>
+                    <img src="public/images/img_settings_base_2.svg" alt="Settings" class="settings" />
+                </label>
+            </div>
+        </div>
+            </div>
+        </div>
+    </header>
+    <div class="bountypost">
+    <div class="bountypost-1 container-xs">
+    <div class="rowlineseven">
+    <div>
+    <div class="rowprice-4">
+    <div class="rowbountytitle-4">
+    <p class="bountytitle-4 ui text size-title">Bounty Title</p>
+    <p class="company-4 ui text size-base">Company</p>
+    </div>
+    <p class="theultimate ui text size-title">$$$</p>
+    </div>
+    <div class="lineseven_one-6"></div>
+    </div>
+    </div>
+    <p class="bounty-1 ui text size-base">Bounty Description</p>
+    </div>
+    <div class="bountypost-2">
+    <div class="columnbountytit">
+        <div class="rowprice">
+            <div class="rowbountytitle">
+                <p class="bountytitle ui text">Bounty Title</p>
+                <p class="company ui text ">Company</p>
+            </div>
+                <p class="price ui text">$$$</p>
+        </div>
+        <div class="lineseven_one-2"></div>
+                <p class="bounty-2 ui text">Bounty Description</p>
+        </div>
+        <div class="bountypost-3">
+            <div class="rowprice">
+                <div class="rowbountytitle">
+                    <p class="bounytitle ui text">Bounty Title</p>
+                    <p class="company ui text">Company</p>
+                </div>
+                    <p class="price ui text">$$$</p>
+                </div>
+                <div class="lineseven_one-2"></div>
+                    <p class="bounty-2 ui text">Bounty Description</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+@include('footer')
 </html>
