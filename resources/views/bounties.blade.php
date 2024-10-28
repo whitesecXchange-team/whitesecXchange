9a47701e04a9676a2c78bdd012abec040414431e
IWantToUseGitHub <110896712+IWantToUseGitHub@users.noreply.github.com>
2024-10-28 23:09:32 +0700

    Fixed bounties.blade.php

--- resources/views/bounties.blade.php
+++ resources/views/bounties.blade.php
@@ -1,69 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    @include('header')
-    <link rel="stylesheet" href="{{ asset('css/bounty.css') }}">
-    <title>White Hat</title>
-</head>
-@include('nav')
-<body>
-    <div class="bounty">
-    <header>
-        <div class="column_one">
-            <div class="columnsearch container-xs">
-                <label class="search ui input gray_600 size-md underline square">
-                    <img src="public/images/img_settings_base_1.svg" alt="Settings" class="settings" />
-                    <input name="search" type="text"/>
-                    <img src="public/images/img_settings_base_2.svg" alt="Settings" class="settings" />
-                </label>
+    <head> @include('header')
+        <link rel="stylesheet" href="{{ asset('css/bounty.css') }}">
+        <title>White Hat</title>
+    </head>
+    @include('nav')
+    <body>
+        <a href="bounties" class="button">
+            <div class="bounty">
+                <h2 class="bounty-title">My calculator is looping infinitely!</h2>
+                <p class="company">Script Kiddie</p>
+                <p class="money">$$$</p>
+                <hr>
+                <p class="bounty-description">Hey man, uhh I just started coding recently, and it isn't going well... Can you fix this issue I sent? For some reason the calculations don't add up, for example 2 + 2 = 8, and 3 + 3 = 18. I'm sure I followed the youtube tutorial properly, I even asked my classmate to review it for me, and he didn't find any abnormalities!</p>
             </div>
-        </div>
+        </a>
+        <a href="bounties" class="button">
+            <div class="bounty">
+                <h2 class="bounty-title">Giveaway!!! [No Bug Bounty Required!!!]</h2>
+                <p class="company">Trustworthy Man</p>
+                <p class="money">$$$</p>
+                <hr>
+                <p class="bounty-description">Last one to visit this page receives 100 simoleons!</p>
             </div>
-        </div>
-    </header>
-    <div class="bountypost">
-    <div class="bountypost-1 container-xs">
-    <div class="rowlineseven">
-    <div>
-    <div class="rowprice-4">
-    <div class="rowbountytitle-4">
-    <p class="bountytitle-4 ui text size-title">Bounty Title</p>
-    <p class="company-4 ui text size-base">Company</p>
-    </div>
-    <p class="theultimate ui text size-title">$$$</p>
-    </div>
-    <div class="lineseven_one-6"></div>
-    </div>
-    </div>
-    <p class="bounty-1 ui text size-base">Bounty Description</p>
-    </div>
-    <div class="bountypost-2">
-    <div class="columnbountytit">
-        <div class="rowprice">
-            <div class="rowbountytitle">
-                <p class="bountytitle ui text">Bounty Title</p>
-                <p class="company ui text ">Company</p>
+        </a>
+        <a href="bounties" class="button">
+                <div class="bounty">
+                <h2 class="bounty-title">Test Post</h2>
+                <p class="company">OwO</p>
+                <p class="money">$$$</p>
+                <hr>
+                <p class="bounty-description">Ayy lmao ( ͡° ͜ʖ ͡°)</p>
             </div>
-                <p class="price ui text">$$$</p>
-        </div>
-        <div class="lineseven_one-2"></div>
-                <p class="bounty-2 ui text">Bounty Description</p>
-        </div>
-        <div class="bountypost-3">
-            <div class="rowprice">
-                <div class="rowbountytitle">
-                    <p class="bounytitle ui text">Bounty Title</p>
-                    <p class="company ui text">Company</p>
-                </div>
-                    <p class="price ui text">$$$</p>
-                </div>
-                <div class="lineseven_one-2"></div>
-                    <p class="bounty-2 ui text">Bounty Description</p>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-@include('footer')
+        </a>
+    </body>
+    @include('footer')
 </html>
